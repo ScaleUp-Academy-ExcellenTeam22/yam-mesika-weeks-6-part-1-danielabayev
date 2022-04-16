330cd774d583c23d987c2c8700c52eebab4f541e
daniel or abayev <dan5204@gmail.com>
2022-04-17 01:14:43 +0300

    Add documentation.

--- stay_positive.py
+++ stay_positive.py
@@ -1,4 +1,8 @@
-def get_positive_numbers():
+def get_positive_numbers() -> list:
+    """
+    This function read numbers from the user separate by comma and return the positive numbers.
+    :return: the positive numbers.
+    """
     numbers = input("enter the numbers:")
     numbers = numbers.split(",")
     numbers = [number for number in numbers if int(number) > 0]
